7524d9f516e47ef7ae6c620510a32f279f6e0d2c
Francesco <76690900+MrZukasa@users.noreply.github.com>
2021-01-05 14:54:18 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MasterBin = New System.Windows.Forms.Button()
+        Me.masterfolder = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.SlaveBin = New System.Windows.Forms.Button()
+        Me.MasterSW = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.SlaveSW = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MasterBin
+        '
+        Me.MasterBin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(88, Byte), Integer))
+        Me.MasterBin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(176, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.MasterBin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.MasterBin.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MasterBin.Location = New System.Drawing.Point(12, 35)
+        Me.MasterBin.Name = "MasterBin"
+        Me.MasterBin.Size = New System.Drawing.Size(115, 56)
+        Me.MasterBin.TabIndex = 0
+        Me.MasterBin.Text = "Master"
+        Me.MasterBin.UseVisualStyleBackColor = True
+        '
+        'masterfolder
+        '
+        Me.masterfolder.FileName = "OpenFileDialog1"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(98, Byte), Integer))
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Location = New System.Drawing.Point(146, 37)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(556, 54)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(98, Byte), Integer))
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.Location = New System.Drawing.Point(146, 110)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(556, 59)
+        Me.TextBox3.TabIndex = 5
+        '
+        'SlaveBin
+        '
+        Me.SlaveBin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(88, Byte), Integer))
+        Me.SlaveBin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(176, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.SlaveBin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.SlaveBin.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SlaveBin.Location = New System.Drawing.Point(12, 108)
+        Me.SlaveBin.Name = "SlaveBin"
+        Me.SlaveBin.Size = New System.Drawing.Size(115, 61)
+        Me.SlaveBin.TabIndex = 3
+        Me.SlaveBin.Text = "Slave"
+        Me.SlaveBin.UseVisualStyleBackColor = True
+        '
+        'MasterSW
+        '
+        Me.MasterSW.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(88, Byte), Integer))
+        Me.MasterSW.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(176, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.MasterSW.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.MasterSW.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MasterSW.Location = New System.Drawing.Point(12, 186)
+        Me.MasterSW.Name = "MasterSW"
+        Me.MasterSW.Size = New System.Drawing.Size(115, 166)
+        Me.MasterSW.TabIndex = 6
+        Me.MasterSW.Text = "Master SW"
+        Me.MasterSW.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(98, Byte), Integer))
+        Me.TextBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox5.Location = New System.Drawing.Point(146, 186)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(556, 166)
+        Me.TextBox5.TabIndex = 7
+        '
+        'TextBox6
+        '
+        Me.TextBox6.BackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(98, Byte), Integer))
+        Me.TextBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox6.Location = New System.Drawing.Point(146, 376)
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(556, 168)
+        Me.TextBox6.TabIndex = 9
+        '
+        'SlaveSW
+        '
+        Me.SlaveSW.BackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.SlaveSW.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(88, Byte), Integer))
+        Me.SlaveSW.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(176, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.SlaveSW.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.SlaveSW.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SlaveSW.Location = New System.Drawing.Point(12, 376)
+        Me.SlaveSW.Name = "SlaveSW"
+        Me.SlaveSW.Size = New System.Drawing.Size(115, 168)
+        Me.SlaveSW.TabIndex = 8
+        Me.SlaveSW.Text = "Slave SW"
+        Me.SlaveSW.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(708, 37)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(40, 35)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(70, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(757, 571)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.SlaveSW)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.MasterSW)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.SlaveBin)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.MasterBin)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Valar Morghulis"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MasterBin As Button
+    Friend WithEvents masterfolder As OpenFileDialog
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents SlaveBin As Button
+    Friend WithEvents MasterSW As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents SlaveSW As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,311 @@
+﻿Imports System.IO
+Imports System.IO.File
+Imports System.Text.Encoding
+Imports System.Text.UnicodeEncoding
+
+Public Class Form1
+    Dim decimali1slave As New List(Of Integer)
+    Dim decimali2slave As New List(Of Integer)
+    Dim decimali3slave As New List(Of Integer)
+    '_______________________________________     
+    Dim decimali1master As New List(Of Integer)
+    Dim decimali2master As New List(Of Integer)
+    Dim decimali3master As New List(Of Integer)
+    Dim playstatus As Boolean = True
+    '_______________________________________    
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles MasterBin.Click
+        masterfolder.Filter = "Binary | *.bin"
+        masterfolder.Title = "Open Master File"
+        'masterfolder.ShowHelp = True
+        masterfolder.FileName = ""
+        If masterfolder.ShowDialog = DialogResult.OK Then
+            Dim fs As New FileStream(masterfolder.FileName, FileMode.Open)
+            Dim binaryreader As New BinaryReader(fs)
+            fs.Position = 0
+            Dim bytes() As Byte = binaryreader.ReadBytes(fs.Length)
+            Dim integers As String = BitConverter.ToString(bytes, 0)
+            Dim replace As String = integers.Replace("-", "")
+            Dim caratteri As Char() = replace
+            '_______________________________________
+            Dim interi As New List(Of String)
+            Dim seconda As New List(Of String)
+            Dim terza As New List(Of String)
+            '_______________________________________
+
+            For i As Integer = 0 To 95
+                If i <= 31 Then
+                    interi.Add(caratteri(i) + caratteri(i + 1))
+                End If
+                If (i > 31) And (i < 64) Then
+                    seconda.Add(caratteri(i) + caratteri(i + 1))
+                ElseIf i > 63 Then
+                    terza.Add(caratteri(i) + caratteri(i + 1))
+                End If
+                i += 1
+            Next i
+
+            TextBox2.Text = "--> " + String.Join("", interi) + " <--" + vbCrLf _
+                + "--> " + String.Join("", seconda) + " <--" + vbCrLf _
+                + "--> " + String.Join("", terza) + " <--"
+            binaryreader.Close()
+            fs.Dispose()
+            MasterSW.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles SlaveBin.Click
+
+        masterfolder.Filter = "Binary | *.bin"
+        masterfolder.Title = "Open Slave File"
+        'masterfolder.ShowHelp = True
+        masterfolder.FileName = ""
+
+        If masterfolder.ShowDialog = DialogResult.OK Then
+            Dim fs As New FileStream(masterfolder.FileName, FileMode.Open)
+            Dim binaryreader As New BinaryReader(fs)
+            fs.Position = 0
+            Dim bytes() As Byte = binaryreader.ReadBytes(fs.Length)
+            Dim integers As String = BitConverter.ToString(bytes, 0)
+            Dim replace As String = integers.Replace("-", "")
+            Dim caratteri As Char() = replace
+            TextBox3.Text = replace
+            '_______________________________________
+            Dim interi As New List(Of String)
+            Dim seconda As New List(Of String)
+            Dim terza As New List(Of String)
+            '_______________________________________
+
+            For i As Integer = 0 To 95
+                If i <= 31 Then
+                    interi.Add(caratteri(i) + caratteri(i + 1))
+                End If
+                If (i > 31) And (i < 64) Then
+                    seconda.Add(caratteri(i) + caratteri(i + 1))
+                ElseIf i > 63 Then
+                    terza.Add(caratteri(i) + caratteri(i + 1))
+                End If
+                i += 1
+            Next i
+
+            TextBox3.Text = "--> " + String.Join("", interi) + " <--" + vbCrLf _
+                + "--> " + String.Join("", seconda) + " <--" + vbCrLf _
+                + "--> " + String.Join("", terza) + " <--"
+            binaryreader.Close()
+            fs.Dispose()
+            SlaveSW.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles MasterSW.Click
+        Dim checksum, checksum2, checksum3, i, sum, sum2, sum3 As Integer
+        Dim line(2000), formattazione, temp, temp2, temp3, intestazione, intestazione2, intestazione3, checksumhex, checksumhex2, checksumhex3 As String
+        Dim stringtemp, stringtemp2, stringtemp3 As New List(Of String)
+
+        masterfolder.Filter = "Hex | *.Hex"
+        masterfolder.Title = "Open Hex File"
+        masterfolder.FileName = ""
+        'masterfolder.ShowHelp = True
+
+
+        'leggo dal file .hex
+        If masterfolder.ShowDialog = DialogResult.OK Then
+            Dim reader = File.OpenText(masterfolder.FileName)
+            Dim righe As Integer = File.ReadAllLines(masterfolder.FileName).Length - 1
+            For i = 0 To righe
+                line(i) = reader.ReadLine()
+            Next i
+
+            'ricavo il path di dove salvare i file
+            Dim directory As String
+            directory = Path.GetDirectoryName(masterfolder.FileName)
+            'MsgBox(directory)
+
+            intestazione = line(1001).Substring(1, 8)
+            intestazione2 = line(1002).Substring(1, 8)
+            intestazione3 = line(1003).Substring(1, 8)
+
+            decimali1master.Clear()
+            decimali2master.Clear()
+            decimali3master.Clear()
+
+            temp = intestazione + TextBox2.Text.ToString.Substring(4, 32)
+            temp2 = intestazione2 + TextBox2.Text.ToString.Substring(46, 32)
+            temp3 = intestazione3 + TextBox2.Text.ToString.Substring(88, 32)
+
+            'calcolo checksum
+            For i = 0 To 39
+                stringtemp.Add(temp(i) + temp(i + 1))
+                stringtemp2.Add(temp2(i) + temp2(i + 1))
+                stringtemp3.Add(temp3(i) + temp3(i + 1))
+                i += 1
+            Next i
+
+            For j As Integer = 0 To 19
+                decimali1master.Add(Integer.Parse(stringtemp(j), Globalization.NumberStyles.HexNumber))
+                decimali2master.Add(Integer.Parse(stringtemp2(j), Globalization.NumberStyles.HexNumber))
+                decimali3master.Add(Integer.Parse(stringtemp3(j), Globalization.NumberStyles.HexNumber))
+                sum = decimali1master(j) + sum
+                sum2 = decimali2master(j) + sum2
+                sum3 = decimali3master(j) + sum3
+            Next j
+
+            checksum = -(sum Mod 256)
+            checksum = checksum + 256
+            checksumhex = Hex(checksum)
+
+            checksum2 = -(sum2 Mod 256)
+            checksum2 = checksum2 + 256
+            checksumhex2 = Hex(checksum2)
+
+            checksum3 = -(sum3 Mod 256)
+            checksum3 = checksum3 + 256
+            checksumhex3 = Hex(checksum3)
+
+            formattazione = "File Originale" + vbCrLf + "--------------------------------------------------------------------------------------" + vbCrLf + line(1001) + vbCrLf + line(1002) _
+                     + vbCrLf + line(1003) + vbCrLf + "--------------------------------------------------------------------------------------" + vbCrLf + "File Aggiornato" + vbCrLf _
+                     + "--------------------------------------------------------------------------------------" + vbCrLf + ":" + temp + checksumhex + vbCrLf + ":" + temp2 + checksumhex2 + vbCrLf _
+                     + ":" + temp3 + checksumhex3 + vbCrLf + "--------------------------------------------------------------------------------------"
+            TextBox5.Text = formattazione
+
+            'creo il nuovo file .hex
+            Dim writer As System.IO.StreamWriter
+            writer = My.Computer.FileSystem.OpenTextFileWriter(directory + "\Master SW " + DateString + ".hex", False, ASCII)
+            line(1001) = ":" + temp + checksumhex
+            line(1002) = ":" + temp2 + checksumhex2
+            line(1003) = ":" + temp3 + checksumhex3
+            For i = 0 To righe
+                writer.WriteLine(line(i))
+            Next i
+
+            'chiudo i due file
+            writer.Close()
+            reader.Close()
+            'reader = File.OpenText(directory + "\Master SW " + DateString + ".hex")
+            'righe = File.ReadAllLines(masterfolder.FileName).Length - 1
+            'line(0) = reader.ReadLine()
+            'MsgBox(line(0))
+
+            MsgBox("Quest'uomo ha creato un file con successo in " + directory, MsgBoxStyle.Information)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.MaximizeBox = False
+        MasterSW.Enabled = False
+        SlaveSW.Enabled = False
+        Try
+            My.Computer.Audio.Play("\\Itvnr01fs3\alit_dept\ALIT_ENG_G\SCAMBI\Boasso\ost.wav")
+            playstatus = True
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles SlaveSW.Click
+
+        Dim sum, sum2, sum3, checksum, checksum2, checksum3 As Integer
+        Dim formattazione, intestazione, intestazione2, intestazione3, temp, temp2, temp3, checksumhex, checksumhex2, checksumhex3 As String
+        Dim stringtemp, stringtemp2, stringtemp3 As New List(Of String)
+        Dim i As Integer
+
+        masterfolder.Filter = "Hex | *.Hex"
+        masterfolder.Title = "Open Hex File"
+        masterfolder.FileName = ""
+        'masterfolder.ShowHelp = True
+
+
+        If masterfolder.ShowDialog = DialogResult.OK Then
+            Dim reader = File.OpenText(masterfolder.FileName)
+            Dim line(2000) As String
+            Dim righe As Integer = File.ReadAllLines(masterfolder.FileName).Length - 1
+            For i = 0 To righe
+                line(i) = reader.ReadLine()
+            Next i
+
+            'ricavo il path di dove salvare i file
+            Dim directory As String
+            directory = Path.GetDirectoryName(masterfolder.FileName)
+            'MsgBox(directory)
+
+            intestazione = line(1001).Substring(1, 8)
+            intestazione2 = line(1002).Substring(1, 8)
+            intestazione3 = line(1003).Substring(1, 8)
+
+            decimali1master.Clear()
+            decimali2master.Clear()
+            decimali3master.Clear()
+
+            temp = intestazione + TextBox3.Text.ToString.Substring(4, 32)
+            temp2 = intestazione2 + TextBox3.Text.ToString.Substring(46, 32)
+            temp3 = intestazione3 + TextBox3.Text.ToString.Substring(88, 32)
+
+            'calcolo checksum
+            For i = 0 To 39
+                stringtemp.Add(temp(i) + temp(i + 1))
+                stringtemp2.Add(temp2(i) + temp2(i + 1))
+                stringtemp3.Add(temp3(i) + temp3(i + 1))
+                i += 1
+            Next i
+
+            For j As Integer = 0 To 19
+                decimali1slave.Add(Integer.Parse(stringtemp(j), Globalization.NumberStyles.HexNumber))
+                decimali2slave.Add(Integer.Parse(stringtemp2(j), Globalization.NumberStyles.HexNumber))
+                decimali3slave.Add(Integer.Parse(stringtemp3(j), Globalization.NumberStyles.HexNumber))
+                sum = decimali1slave(j) + sum
+                sum2 = decimali2slave(j) + sum2
+                sum3 = decimali3slave(j) + sum3
+            Next j
+
+            checksum = -(sum Mod 256)
+            checksum = checksum + 256
+            checksumhex = Hex(checksum)
+
+            checksum2 = -(sum2 Mod 256)
+            checksum2 = checksum2 + 256
+            checksumhex2 = Hex(checksum2)
+
+            checksum3 = -(sum3 Mod 256)
+            checksum3 = checksum3 + 256
+            checksumhex3 = Hex(checksum3)
+
+            formattazione = "File Originale" + vbCrLf + "--------------------------------------------------------------------------------------" + vbCrLf + line(1001) + vbCrLf + line(1002) _
+                     + vbCrLf + line(1003) + vbCrLf + "--------------------------------------------------------------------------------------" + vbCrLf + "File Aggiornato" + vbCrLf _
+                     + "--------------------------------------------------------------------------------------" + vbCrLf + ":" + temp + checksumhex + vbCrLf + ":" + temp2 + checksumhex2 + vbCrLf _
+                     + ":" + temp3 + checksumhex3 + vbCrLf + "--------------------------------------------------------------------------------------"
+            TextBox6.Text = formattazione
+
+            'creo il nuovo file .hex
+            Dim writer As System.IO.StreamWriter
+            writer = My.Computer.FileSystem.OpenTextFileWriter(directory + "\Slave SW " + DateString + ".hex", False, ASCII)
+            line(1001) = ":" + temp + checksumhex
+            line(1002) = ":" + temp2 + checksumhex2
+            line(1003) = ":" + temp3 + checksumhex3
+            For i = 0 To righe
+                writer.WriteLine(line(i))
+            Next i
+
+            'chiudo i due file
+            writer.Close()
+            reader.Close()
+            MsgBox("Quest'uomo ha creato un file con successo in " + directory, MsgBoxStyle.Information)
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        If playstatus = False Then
+            Try
+                My.Computer.Audio.Play("\\Itvnr01fs3\alit_dept\ALIT_ENG_G\SCAMBI\Boasso\ost.wav")
+                playstatus = True
+            Catch ex As Exception
+
+            End Try
+        Else
+            Try
+                My.Computer.Audio.Stop()
+                playstatus = False
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+End Class
